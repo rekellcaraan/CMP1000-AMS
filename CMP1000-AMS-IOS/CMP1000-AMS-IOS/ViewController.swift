2bb3ffb60f6e1313185fdf621e91d416cdb4c654
Rekell Caraan <caraanr@mcgi.org>
2022-12-15 18:00:26 -0500

    Initial Project Creation

---
+++ CMP1000-AMS-IOS/CMP1000-AMS-IOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CMP1000-AMS-IOS
+//
+//  Created by REKELL CARAAN on 2022-12-15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
