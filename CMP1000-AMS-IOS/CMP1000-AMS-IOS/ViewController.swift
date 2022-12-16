8995a8125729773bb75f4a47a0a0e6e3f02c8de9
Rekell Caraan <caraanr@mcgi.org>
2022-12-16 07:23:32 -0500

    Added CoreData Context Saving and Retrieving of Users

--- CMP1000-AMS-IOS/CMP1000-AMS-IOS/ViewController.swift
+++ CMP1000-AMS-IOS/CMP1000-AMS-IOS/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let amsCDContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    func getCurrentSignedUserRole(userName: String) -> String
+    {
+        do
+        {
+            let users = try amsCDContext.fetch(Users.fetchRequest())
+        }
+        catch
+        {
+            
+        }
+        
+        return ""
+    }
+    
+    func saveCurrentSignedUser(userName: String, userRole: String, userPassword: String)
+    {
+        let newUser = Users(context: amsCDContext)
+        newUser.username = userName
+        newUser.role = userRole
+        newUser.password = userPassword
+        
+        do{
+            try amsCDContext.save()
+        }
+        catch
+        {
+        }
+            
+    }
 }
 
